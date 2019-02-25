ae3b320ed9db816c62bf90dcc5f14ba61f387329
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2019-02-25 14:09:06 -0700

    Adding tensorflow worker.

--- tensorflow/data_download.sh
+++ tensorflow/data_download.sh
@@ -22,8 +22,3 @@ do
 		wget "http://yann.lecun.com/exdb/mnist/${file}"
 fi
 done
-
-#wget "http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz"
-#wget "http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz"
-#wget "http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz"
-#wget "http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz"
