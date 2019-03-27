e53ed04dbaa8a6a4bd4cf55a1daf09a91b66185e
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2019-03-27 08:51:34 -0600

    Working on smart3seq pipeline.

--- smart3seq/scripts/smart3seq_alignment.sh
+++ smart3seq/scripts/smart3seq_alignment.sh
@@ -3,3 +3,5 @@
 set -e
 
 echo "Executing Smart3seq alignment"
+cd /usr/local/3SEQtools
+align_smart-3seq.sh /data/genome/hg38/star/dbsnp147_gencode25-68 /data/smart3seq/*fastq.gz
--- smart3seq/scripts/smart3seq_counting.sh
+++ smart3seq/scripts/smart3seq_counting.sh
@@ -3,3 +3,5 @@
 set -e
 
 echo "Executing Smart3seq read counting"
+cd /usr/local/3SEQtools
+Rscript make_expression_table.R --no-rlog /data/genome/hg38/gencode.v25.annotation.gtf /data/smart3seq/tophat_out/*bam
