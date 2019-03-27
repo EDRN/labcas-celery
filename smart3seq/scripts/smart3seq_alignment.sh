4d22c00e2b77b4698fe7c37c6dded51795b421aa
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2019-03-27 09:34:34 -0600

    Updating smart3seq scripts

--- smart3seq/scripts/smart3seq_alignment.sh
+++ smart3seq/scripts/smart3seq_alignment.sh
@@ -4,4 +4,4 @@ set -e
 
 echo "Executing Smart3seq alignment"
 cd /usr/local/3SEQtools
-align_smart-3seq.sh /data/genome/hg38/star/dbsnp147_gencode25-68 /data/smart3seq/*fastq.gz
+align_smart-3seq.sh /data/smart3seq/genome/hg38/star/dbsnp147_gencode25-68 /data/smart3seq/data/*fastq.gz
--- smart3seq/scripts/smart3seq_counting.sh
+++ smart3seq/scripts/smart3seq_counting.sh
@@ -4,4 +4,5 @@ set -e
 
 echo "Executing Smart3seq read counting"
 cd /usr/local/3SEQtools
-Rscript make_expression_table.R --no-rlog /data/genome/hg38/gencode.v25.annotation.gtf /data/smart3seq/tophat_out/*bam
+Rscript make_expression_table.R --no-rlog /data/smart3seq/genome/hg38/gencode.v25.annotation.gtf /data/smart3seq/data/tophat_out/*bam
+
