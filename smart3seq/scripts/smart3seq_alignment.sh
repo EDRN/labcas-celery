09907335287e109f3c5116001c692025fcd898a1
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2019-04-04 06:19:20 -0600

    Using output/ directory

--- smart3seq/scripts/smart3seq_alignment.sh
+++ smart3seq/scripts/smart3seq_alignment.sh
@@ -6,8 +6,8 @@ set -e
 run=$1
 echo "Executing Smart3seq alignment for run: $run"
 
-mkdir -p ${SMART3SEQ_DATA_DIR}/data/${run}
-cd ${SMART3SEQ_DATA_DIR}/data/${run}
+mkdir -p ${SMART3SEQ_DATA_DIR}/output/${run}
+cd ${SMART3SEQ_DATA_DIR}/output/${run}
 #align_smart-3seq.sh ${SMART3SEQ_DATA_DIR}/genome/hg38/star/dbsnp147_gencode25-68 ${SMART3SEQ_DATA_DIR}/data/${run}/*fastq.gz
 # FIXME: replace 11553 -> ${run}
 align_smart-3seq.sh ${SMART3SEQ_DATA_DIR}/genome/hg38/star/dbsnp147_gencode25-68 ${SMART3SEQ_DATA_DIR}/data/11553/*fastq.gz
--- smart3seq/scripts/smart3seq_counting.sh
+++ smart3seq/scripts/smart3seq_counting.sh
@@ -5,5 +5,5 @@ set -e
 run=$1
 echo "Executing Smart3seq read counting for run: $run"
 
-cd ${SMART3SEQ_DATA_DIR}/data/${run}
-Rscript /usr/local/3SEQtools/make_expression_table.R --no-rlog ${SMART3SEQ_DATA_DIR}/genome/hg38/gencode.v25.annotation.gtf ${SMART3SEQ_DATA_DIR}/data/${run}/*bam
+cd ${SMART3SEQ_DATA_DIR}/output/${run}
+Rscript /usr/local/3SEQtools/make_expression_table.R --no-rlog ${SMART3SEQ_DATA_DIR}/genome/hg38/gencode.v25.annotation.gtf ${SMART3SEQ_DATA_DIR}/output/${run}/*bam
