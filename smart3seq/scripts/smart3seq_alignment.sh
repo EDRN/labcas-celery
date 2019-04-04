b45ee4810ef46fef6e22db4af562b64abdca1ffa
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2019-04-04 14:19:24 -0600

    Changing the smart3seq pipeline

--- smart3seq/scripts/smart3seq_alignment.sh
+++ smart3seq/scripts/smart3seq_alignment.sh
@@ -9,5 +9,7 @@ echo "Executing Smart3seq alignment for run: $run"
 mkdir -p ${SMART3SEQ_DATA_DIR}/output/${run}
 cd ${SMART3SEQ_DATA_DIR}/output/${run}
 #align_smart-3seq.sh ${SMART3SEQ_DATA_DIR}/genome/hg38/star/dbsnp147_gencode25-68 ${SMART3SEQ_DATA_DIR}/data/${run}/*fastq.gz
-# FIXME: replace 11553 -> ${run}
-align_smart-3seq.sh ${SMART3SEQ_DATA_DIR}/genome/hg38/star/dbsnp147_gencode25-68 ${SMART3SEQ_DATA_DIR}/data/11553/*fastq.gz
+#align_smart-3seq.sh ${SMART3SEQ_DATA_DIR}/genome/hg38/star/dbsnp147_gencode25-68 ${SMART3SEQ_DATA_DIR}/data/11553/*fastq.gz
+# FIXME:
+cp -R ${SMART3SEQ_DATA_DIR}/genome .
+align_smart-3seq.sh ./hg38/star/dbsnp147_gencode25-68 ${SMART3SEQ_DATA_DIR}/data/11553/*fastq.gz
