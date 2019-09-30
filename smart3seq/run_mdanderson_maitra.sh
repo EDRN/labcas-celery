59518fa0d05139035747099c51788d8fa158011b
cinquini <luca.cinquini@jpl.nasa.gov>
2019-09-30 10:14:10 +0000

    Adding config files for MDAnderson Maitra Smart3seq

---
+++ smart3seq/run_mdanderson_maitra.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Runs a Smart-3Seq pipeline for a specific set of samples
+
+CONFIG_FILE=/usr/local/edrn/src/labcas-celery/smart3seq/config-mdanderson-maitra.yaml
+PROJECT_DIR=/efs/docker/labcas/mcl/archive/Smart-3Seq/MDAnderson_Maitra
+
+docker run -it -v $CONFIG_FILE:/usr/local/SMART-3SEQ-smk/code/config.yaml\
+               -v /efs/docker/labcas/smart3seq/star_ref:/star_ref\
+               -v $PROJECT_DIR:/project\
+               edrn/labcas-smart3seq:latest sh -c "source activate smart-3seq && snakemake --snakefile pipeline.smk --configfile config.yaml -j 4"
