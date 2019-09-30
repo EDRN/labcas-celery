abbf0a39eeaaa65f77e9e699c1f0848c38419668
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2019-09-30 11:02:01 -0600

    Fixing s/d bug

--- smart3seq/run_mdanderson_maitra.sh
+++ smart3seq/run_mdanderson_maitra.sh
@@ -4,7 +4,7 @@
 CONFIG_FILE=/usr/local/edrn/src/labcas-celery/smart3seq/config-mdanderson-maitra.yaml
 PROJECT_DIR=/efs/docker/labcas/mcl/archive/Smart-3Seq/MDAnderson_Maitra
 
-docker run -its -v $CONFIG_FILE:/usr/local/SMART-3SEQ-smk/code/config.yaml\
+docker run -itd -v $CONFIG_FILE:/usr/local/SMART-3SEQ-smk/code/config.yaml\
                 -v /efs/docker/labcas/smart3seq/star_ref:/star_ref\
                 -v $PROJECT_DIR:/project\
                 edrn/labcas-smart3seq:latest sh -c "source activate smart-3seq && snakemake --snakefile pipeline.smk --configfile config.yaml -j 4"
