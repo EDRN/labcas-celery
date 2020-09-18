3d2f7f71c1f78728a55089430429b2de84ccaacb
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2020-09-18 09:16:57 -0600

    One more task

--- airflow/dags/unmcpc.py
+++ airflow/dags/unmcpc.py
@@ -34,11 +34,22 @@ t1 = BashOperator(
     task_id='download_data_from_s3',
     bash_command=('aws s3 sync'
                   ' {{ dag_run.conf["input"] }}'
-                  ' /tmp/input_data/2020'
+                  ' /efs-ecs/docker/labcas/unmcpc/input_data/2020'
                   ' --profile {{ var.value.saml_profile }}'),
     dag=dag)
 
-t2 = DockerOperator(
+t2 = BashOperator(
+    task_id='submit_k8s_job',
+    environment={
+                 "INPUT_DATA": "{{ params.input }}",
+                 "OUTPUT_DATA": "{{ params.output }}" 
+                },
+    # IMPORTANT: must have space after the .sh!
+    # bash_command='{{ dag_run.conf["dags_folder"] }}/submit_k8s_job.sh ',
+    bash_command='{{ var.value.k8s_home }}/submit_k8s_job.sh ',
+    dag=dag)
+'''
+t2_ = DockerOperator(
                 task_id='helloworld_from_docker',
                 image='centos:7',
                 api_version='auto',
@@ -47,8 +58,8 @@ t2 = DockerOperator(
                 environment={
                         'HELLO_HOME': "/hello",
                 },
-                volumes=['/tmp/input_data/2020/:/input_data/2020',
-                         '/tmp/output_data/:/output_data'],
+                volumes=['/efs-ecs/docker/labcas/unmcpc/input_data/2020/:/input_data/2020',
+                         '/efs-ecs/docker/labcas/unmcpc/output_data/:/output_data'],
                 #command="bash -c 'for i in {1..5}; do echo hi; sleep 2; done'",
                 command='cp -R /input_data/2020 /output_data/.',
                 docker_url='unix://var/run/docker.sock',
@@ -56,12 +67,12 @@ t2 = DockerOperator(
                 default_args=default_args,
                 dag=dag
         )
-
+'''
 
 t3 = BashOperator(
        task_id='upload_data_to_s3',
        bash_command=('aws s3 sync'
-                     ' /tmp/output_data/2020'
+                     ' /efs-ecs/docker/labcas/unmcpc/output_data/2020'
                      ' {{ dag_run.conf["output"] }}'
                      ' --profile {{ var.value.saml_profile }}'),
     dag=dag)
